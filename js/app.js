1d005784d72eac949b66cfa8560c4859a55c4675
Wenjie <wenjietj@gmail.com>
2021-01-10 23:47:36 +0800

    refactor velocity calculation into helper func

--- js/app.js
+++ js/app.js
@@ -1,149 +1,177 @@
-////////////////////////
-// DOM Ready
-////////////////////////
+// ////////////////////////
+// // DOM Ready
+// ////////////////////////
 
-function ready(callbackFunc) {
-	if (document.readyState !== 'loading') {
-		callbackFunc();
-	} else if (document.addEventListener) {
-		document.addEventListener('DOMContentLoaded', callbackFunc);
+// function ready(callbackFunc) {
+// 	if (document.readyState !== 'loading') {
+// 		callbackFunc();
+// 	} else if (document.addEventListener) {
+// 		document.addEventListener('DOMContentLoaded', callbackFunc);
+// 	}
+// }
+
+// ready(function () {
+
+// });
+
+//////////////////////
+// Init Canvas
+/////////////////////
+
+const canvas = document.querySelector('canvas');
+
+// set canvas width height same as windows
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+// get 2d drawing context
+const c = canvas.getContext('2d');
+
+// player position
+const playerX = canvas.width / 2;
+const playerY = canvas.height;
+
+//////////////////////
+// Helper Functions
+/////////////////////
+
+const drawCircle = (x, y, radius, color) => {
+	c.beginPath();
+	c.arc(x, y, radius, Math.PI * 2, false);
+	c.fillStyle = color;
+	c.fill();
+};
+
+const getVelocitiesXY = (x, y) => {
+	// get angle of trajectory
+	const angle = Math.atan2(x, y);
+	// get velocities and return object
+	return { x: Math.cos(angle), y: Math.sin(angle) };
+};
+
+//////////////////////
+// Player Class
+/////////////////////
+class Player {
+	constructor(radius) {
+		// player appearance
+		this.x = playerX;
+		this.y = playerY;
+		this.radius = radius;
+		this.color = 'gray';
+		// player data
+		this.missileCount = 20;
+		this.missileArray = [];
+		this.score = 0;
+	}
+	render() {
+		drawCircle(this.x, this.y, this.radius, this.color);
+	}
+
+	updateMissiles() {
+		this.missileArray.forEach((missile) => {
+			missile.update();
+		});
 	}
 }
 
-ready(function () {
-	//////////////////////
-	// Init Canvas
-	/////////////////////
-
-	const canvas = document.querySelector('canvas');
-
-	// set canvas width height same as windows
-	canvas.width = innerWidth;
-	canvas.height = innerHeight;
-
-	// get 2d drawing context
-	const c = canvas.getContext('2d');
-
-	// player position
-	const playerX = canvas.width / 2;
-	const playerY = canvas.height;
-
-	//////////////////////
-	// Helper Functions
-	/////////////////////
-
-	const drawCircle = (x, y, radius, color) => {
-		c.beginPath();
-		c.arc(x, y, radius, Math.PI * 2, false);
-		c.fillStyle = color;
-		c.fill();
-	};
-
-	//////////////////////
-	// Player Class
-	/////////////////////
-	class Player {
-		constructor(radius) {
-			// player appearance
-			this.x = playerX;
-			this.y = playerY;
-			this.radius = radius;
-			this.color = 'gray';
-			// player data
-			this.missiles = 20;
-			this.score = 0;
-		}
-		render() {
-			drawCircle(this.x, this.y, this.radius, this.color);
-		}
+//////////////////////
+// Missile Class
+/////////////////////
+class Missile {
+	constructor(x, y, velocityX, velocityY, radius, color) {
+		this.x = x;
+		this.y = y;
+		this.velocityX = velocityX;
+		this.velocityY = velocityY;
+		this.radius = radius;
+		this.color = color;
 	}
 
-	//////////////////////
-	// Missile Class
-	/////////////////////
-	class Missile {
-		constructor(x, y, velocityX, velocityY, radius, color) {
-			this.x = x;
-			this.y = y;
-			this.velocityX = velocityX;
-			this.velocityY = velocityY;
-			this.radius = radius;
-			this.color = color;
-		}
-
-		render() {
-			drawCircle(this.x, this.y, this.radius, this.color);
-		}
-
-		update() {
-			this.render();
-			this.x = this.x + this.velocityX;
-			this.y = this.y + this.velocityY;
-		}
+	render() {
+		drawCircle(this.x, this.y, this.radius, this.color);
 	}
 
-	//////////////////////
-	// Enemy Class
-	/////////////////////
+	update() {
+		this.render();
+		this.x = this.x + this.velocityX;
+		this.y = this.y + this.velocityY;
+	}
+}
 
-	class EnemyMissile extends Missile {
-		constructor(x, y, velocityX, velocityY, radius, color) {
-			super(x, y, velocityX, velocityY, radius, color);
-			this.points = 100;
-		}
+//////////////////////
+// Enemy Class
+/////////////////////
+
+class EnemyMissile extends Missile {
+	constructor(x, y, velocityX, velocityY, radius, color) {
+		super(x, y, velocityX, velocityY, radius, color);
+		this.points = 100;
+	}
+}
+
+//////////////////////
+// Enemy Factory Class
+/////////////////////
+
+class EnemyMissileFactory {
+	constructor() {
+		this.enemies = [];
 	}
 
-	//////////////////////
-	// Enemy Factory Class
-	/////////////////////
-
-	class EnemyMissileFactory {
-		constructor() {
-			this.enemies = [];
-		}
-		// setInterval((),2000)
-		spawnEnemy() {
-			this.enemies.push(new EnemyMissile(100, 100, 1, 1, 5, 'red'));
-		}
+	createEnemy() {
+		setInterval(() => {
+			this.enemies.push(new EnemyMissile(100, 0, 1, 1, 5, 'red'));
+		}, 2000);
 	}
 
-	/////// test program /////////
-	const player = new Player(30);
-
-	const missileArray = [];
-
-	const animate = () => {
-		requestAnimationFrame(animate);
-		// refresh canvas
-		c.clearRect(0, 0, canvas.width, canvas.height);
-		// render and update elements
-		player.render();
-		missileArray.forEach((missile) => {
-			missile.update();
+	updateEnemies() {
+		this.enemies.forEach((enemy) => {
+			enemy.update();
 		});
-	};
+	}
+}
 
-	//////////////////////
-	// Event Listeners
-	/////////////////////
+//////////////////////
+// Event Listeners
+/////////////////////
 
-	addEventListener('click', (event) => {
-		// get angle of trajectory
-		const angle = Math.atan2(event.clientY - playerY, event.clientX - playerX);
-		// get velocities
-		let velocityX = Math.cos(angle);
-		let velocityY = Math.sin(angle);
-
-		// on click create a new missile
-		missileArray.push(
-			new Missile(playerX, playerY, velocityX, velocityY, 5, 'blue')
-		);
-	});
-	animate();
+addEventListener('click', (event) => {
+	// get velocities x y of missile
+	const velocities = getVelocitiesXY(
+		event.clientY - playerY,
+		event.clientX - playerX
+	);
+	// on click create a new missile
+	player.missileArray.push(
+		new Missile(playerX, playerY, velocities.x, velocities.y, 5, 'blue')
+	);
 });
 
+/////// test program /////////
+// player obj
+const player = new Player(30);
+// enemy factory obj
+const enemyMissileFactory = new EnemyMissileFactory();
+enemyMissileFactory.createEnemy();
+
+// animate canvas
+const animate = () => {
+	requestAnimationFrame(animate);
+	// refresh canvas
+	c.clearRect(0, 0, canvas.width, canvas.height);
+
+	// render and update elements
+	player.render();
+	player.updateMissiles();
+	enemyMissileFactory.updateEnemies();
+};
+
+animate();
+
 /// To Note/fix:
-// consider returning velocity X and Y as an object
+// make enemies spawn randomly from top and travel to the ground
+// consider putting push missile on click into player object
 // consider how to break it into multiple files
 // consider breaking canvas into an object
 // change enemy shape or overlay sprite and get orientation
