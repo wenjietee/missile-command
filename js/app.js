c6dea5bc46c74519926cfbfff93a9dacee35c9b9
Wenjie <wenjietj@gmail.com>
2021-01-10 16:54:50 +0800

    created player class

--- js/app.js
+++ js/app.js
@@ -23,4 +23,28 @@ ready(function () {
 
 	// get 2d drawing context
 	const c = canvas.getContext('2d');
+
+	//////////////////////
+	// Player Class
+	/////////////////////
+	class Player {
+		constructor(radius) {
+			// init player position at the bottom
+			this.x = canvas.width / 2;
+			this.y = canvas.height;
+			this.radius = radius;
+			this.color = 'tomato';
+		}
+
+		render() {
+			c.beginPath();
+			c.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+			c.fillStyle = this.color;
+			c.fill();
+		}
+	}
+
+	const player = new Player(30);
+
+	player.render();
 });
